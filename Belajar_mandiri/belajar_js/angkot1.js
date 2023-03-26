486701df19678246283a3704164cbfec50ec111b
Fx. Guntur <fxguntur04@gmail.com>
2023-03-26 12:20:06 +0700

    Belajar js

---
+++ Belajar_mandiri/belajar_js/angkot1.js
@@ -0,0 +1,6 @@
+var noSeriAngkot = 1;
+
+while(noSeriAngkot<=10){
+    console.log("Angkot No. "+noSeriAngkot+" beroperasi dengan baik.");
+    noSeriAngkot+=1;
+}
---
+++ Belajar_mandiri/belajar_js/script.js
@@ -0,0 +1,15 @@
+var x = 10;
+alert('hello world!');
+var a = false;
+
+while(a === false){
+    var nama = prompt('masukkan nama : ');
+    a = confirm('kamu yakin?');
+}
+// if(a==true){
+//     alert('nama saya '+nama);
+// } else{
+//     alert('gagal input nama');
+// }
+console.log("Hello World");
+console.log("isi dari variabel x adalah "+x);
